1ef4df9848dba5f7723745ab4c7a64e15a9a4bfe
yuanyufan <yuanyufan@yuanyufan.com>
2025-12-22 16:40:13 +0800

    登录

--- src/main/java/com/hebei/controller/UserController.java
+++ src/main/java/com/hebei/controller/UserController.java
@@ -3,6 +3,8 @@ package com.hebei.controller;
 import com.hebei.pojo.Result;
 import com.hebei.pojo.User;
 import com.hebei.server.UserServer;
+import com.hebei.util.JwtUtil;
+import com.hebei.util.Md5Util;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.extern.slf4j.Slf4j;
@@ -11,6 +13,9 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -33,6 +38,27 @@ public class UserController {
             return Result.success();
         } else {
             return Result.error("用户已存在");
+        }
+    }
+
+
+    /*
+     * 用户登录
+     * */
+    @PostMapping("/login")
+    public Result login(@Pattern(regexp = "^\\S{5,16}$") String username, @Pattern(regexp = "^\\S{5,16}$") String password) {
+        log.info("用户登录：{}", username);
+        //登录验证
+        User user = userServer.findByUsername(username);
+        if (Md5Util.getMD5String(password).equals(user.getPassword())) {
+            //登录，下发JWT
+            Map<String, Object> claims = new HashMap();
+            claims.put("username", user.getUsername());
+            claims.put("id", user.getId());
+            String token = JwtUtil.genToken(claims);
+            return Result.success(token);
+        } else {
+            return Result.error("登录失败");
         }
     }
 }
